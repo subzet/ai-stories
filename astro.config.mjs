82d79ceb69b93679f91a5501b13794c59f831cc3
Manuel Ponsa <manuponsa@gmail.com>
2024-03-18 17:01:13 -0300

    feat: add new story page & placeholder for other pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,13 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import alpinejs from "@astrojs/alpinejs";
 import node from '@astrojs/node';
+import htmx from 'astro-htmx';
 
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), alpinejs()],
+  integrations: [tailwind(), alpinejs(), htmx(), icon()],
   output: "server",
   adapter: node({
     mode: "standalone"
